f73ef87fcf462881cf793c83d75461c8e7b65a2e
Waun Broderick <waun.broderick@shopify.com>
2025-06-01 16:32:55 -0300

    hello world

--- main.ts
+++ main.ts
@@ -20,20 +20,6 @@ import { helper } from './helper';
  * 
  * 
  * 
- * 
- *   
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
  * @controllerDebug
  * The following arrays are available for joining:
  * 1. changedFiles: ['main.ts']
